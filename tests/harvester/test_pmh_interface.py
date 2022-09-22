8c67f4d330b89332287e8bc170da06870618a9c9
helhotti <helmiina.hotti@csc.fi>
2022-09-22 14:29:09 +0300

    KP-6111 Finalized unit tests for fetching METS files and added a helper function

--- tests/harvester/test_pmh_interface.py
+++ tests/harvester/test_pmh_interface.py
@@ -4,7 +4,7 @@ Tests for PMH_API
 
 from harvester.pmh_interface import PMH_API
 import pytest
-
+import builtins
 
 
 def _check_result(ids, expected_info):
@@ -30,30 +30,60 @@ def test_binding_ids_from_two_page_response(oai_pmh_api_url,
     Ensure that binding IDs are extracted correctly from a two-page response.
     """
     api = PMH_API(oai_pmh_api_url)
-    ids = list(api.binding_ids('col-681'))
+    ids = list(api.dc_identifiers('col-681'))
     _check_result(ids, two_page_pmh_response)
 
 
-def test_fetch_mets_with_filename(oai_pmh_api_url,
-                                  mets_binding_id,
-                                  expected_mets_response,
-                                  tmp_path):
+def test_binding_id_from_dc(oai_pmh_api_url,
+                            mets_dc_identifier):
     """
-    Ensure that a valid METS file is fetched with file_name parameter.
+    Test that DC identifiers are parsed to binding IDs correctly.
     """
-    id = mets_binding_id.split('/')[-1]
     api = PMH_API(oai_pmh_api_url)
-    response = api.fetch_mets(mets_binding_id, tmp_path, f"{id}_METS.xml")
+    assert api.binding_id_from_dc(mets_dc_identifier) == "379973"
+
+
+def test_fetch_mets_with_filename(oai_pmh_api_url,
+                                  mets_dc_identifier,
+                                  expected_mets_response,
+                                  tmp_path,
+                                  mocker):
+    """
+    Ensure that a valid METS file is fetched and written to disk with file_name parameter.
+    """
+    api = PMH_API(oai_pmh_api_url)
+    binding_id = api.binding_id_from_dc(mets_dc_identifier)
+    mocker.patch('builtins.open')
+    response = api.fetch_mets(mets_dc_identifier, tmp_path, f"{binding_id}_METS.xml")
+    builtins.open.assert_called_once_with(str(tmp_path/f"{binding_id}_METS.xml"), 'w')
     assert response == expected_mets_response
 
 
 def test_fetch_mets_without_filename(oai_pmh_api_url,
-                                     mets_binding_id,
+                                     mets_dc_identifier,
                                      expected_mets_response,
-                                     tmp_path):
+                                     tmp_path,
+                                     mocker):
     """
-    Ensure that a valid METS file is fetched without file_name parameter.
+    Ensure that a valid METS file is fetched and written to disk without file_name parameter.
     """
     api = PMH_API(oai_pmh_api_url)
-    response = api.fetch_mets(mets_binding_id, tmp_path)
+    binding_id = api.binding_id_from_dc(mets_dc_identifier)
+    mocker.patch('builtins.open')
+    response = api.fetch_mets(mets_dc_identifier, tmp_path)
+    builtins.open.assert_called_once_with(str(tmp_path/f"{binding_id}_METS.xml"), 'w')
     assert response == expected_mets_response
+
+
+def test_fetch_all_mets_for_set(oai_pmh_api_url,
+                                two_page_set_id,
+                                tmp_path,
+                                mocker):
+    """
+    Ensure that fetch_mets is called the correct number of times during a fetch_all_mets_for_set call.
+    """
+    api = PMH_API(oai_pmh_api_url)
+    mocker.patch('harvester.pmh_interface.PMH_API.fetch_mets')
+    mocker.patch('harvester.pmh_interface.PMH_API.dc_identifiers', return_value=range(106))
+    api.fetch_all_mets_for_set(two_page_set_id, tmp_path)
+    assert api.fetch_mets.call_count == 106
