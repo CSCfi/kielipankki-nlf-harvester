229ef07c703a478f1e402f068e20ca2445269104
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-10-05 13:31:22 +0000

    KP-6162 Add CLI for displaying download URLs
    Some refactoring was also done on the side, moving the `--url` away from
    the shared options of the whole group. CommandCollections would have
    been nice for having two collections (one for direct OAI-PMH stuff and
    one for METS stuff) but unfortunately those do not preserve the context
    of the group, so it's simpler to just group all commands to one group
    and copypaste the arguments. It is not too neat, but on the other hand
    we will have a limited number of commands so it should not be critical
    either.

--- harvester_cli.py
+++ harvester_cli.py
@@ -9,33 +9,28 @@ from harvester.mets import METS
 
 
 @click.group()
-@click.pass_context
+def cli():
+    """
+    Harvester for OAI-PMH data.
+    """
+
+
+@cli.command
+@click.argument("set_id")
 @click.option(
     "--url",
     default="https://digi.kansalliskirjasto.fi/interfaces/OAI-PMH",
     help="URL of the OAI-PMH API to be used",
 )
-def cli(ctx, url):  # pylint: disable=unused-argument
-    """
-    Harvester for OAI-PMH data.
-    """
-    ctx.ensure_object(dict)
-
-    api = PMH_API(url)
-    ctx.obj["API"] = api
-
-
-@cli.command
-@click.pass_context
-@click.argument("set_id")
-def binding_ids(ctx, set_id):
+def binding_ids(set_id, url):
     """
     Fetch all binding IDs in the given set.
     """
     # pylint does not understand that variables are inherited from the command
     # group
     # pylint: disable=undefined-variable
-    ids = ctx.obj["API"].dc_identifiers(set_id)
+    api = PMH_API(url)
+    ids = api.dc_identifiers(set_id)
     for id_ in ids:
         click.echo(id_)
 
@@ -46,10 +41,42 @@ def binding_ids(ctx, set_id):
 def checksums(mets_file_path, encoding):
     """
     Output checksums for all files listed in the METS document.
+
+    \b
+    METS_FILE_PATH:
+        Path to the METS file to be read
     """
     mets = METS(mets_file_path, encoding)
     for file in mets.files():
         click.echo(file.checksum)
+
+
+@cli.command
+@click.argument("mets_file_path")
+@click.argument(
+    "collection_dc_identifier",
+)
+@click.option("--encoding", default="utf-8")
+def download_urls(mets_file_path, collection_dc_identifier, encoding):
+    """
+    Print dwnload URLs for all files in METS
+
+    \b
+    METS_FILE_PATH:
+        Path to the METS file to be read
+
+    \b
+    COLLECTION_DC_IDENTIFIER:
+        Dublin Core identifier for the collection to
+        which the binding described by this METS belongs. E.g.
+        https://digi.kansalliskirjasto.fi/sanomalehti/binding/380082.
+    """
+    mets = METS(mets_file_path, encoding)
+    for file in mets.files():
+        try:
+            click.echo(file.download_url(collection_dc_identifier))
+        except NotImplementedError:
+            click.echo(f"No download URL available for file {file.location_xlink}")
 
 
 if __name__ == "__main__":
--- tests/test_harvester_cli.py
+++ tests/test_harvester_cli.py
@@ -19,8 +19,8 @@ def test_binding_ids_with_url(oai_pmh_api_url, two_page_pmh_response, two_page_s
     result = runner.invoke(
         cli,
         [
-            "--url", oai_pmh_api_url,
             "binding-ids",
+            "--url", oai_pmh_api_url,
             two_page_set_id,
         ],
     )
@@ -68,3 +68,22 @@ def test_checksums(simple_mets_path):
         ],
     )
     assert "\n33cbc005ce7dac534bdcc424c8a082cd\n" in result.output
+
+
+def test_download_urls(simple_mets_path):
+    """
+    Check that the CLI is able to call the `download_urls` function.
+
+    At the time of writing this test, we do not have the functionality for identifying
+    different types of files, so the output does not contain relevant information to
+    check. It is advised to edit this test as support is added for new file types.
+    """
+    runner = CliRunner()
+
+    result = runner.invoke(
+        cli,
+        ["download-urls", simple_mets_path, "https://example.com/1234"],
+    )
+
+    # four pages, alto and access image for each, plus a trailing newline
+    assert len(result.output.split("\n")) == 8 + 1
