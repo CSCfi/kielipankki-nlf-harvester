d29c3b3f10f066ead588a389464d268a8d919a35
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-10-07 07:54:49 +0000

    KP-6162 skeleton for file download

--- harvester/file.py
+++ harvester/file.py
@@ -3,6 +3,10 @@ Representation of a single file (e.g. an XML file with OCR results for a
 page from a newspaper, or a jpeg showing the scanned page).
 """
 
+import os
+from pathlib import Path
+import urllib.request
+
 
 class File:
     """
@@ -63,6 +67,59 @@ class File:
         raise NotImplementedError(
             "download_url must be defined separately for each subclass of File"
         )
+
+    def _default_base_path(self):
+        """
+        TODO
+        """
+        return Path(os.getcwd())
+
+    def _default_file_dir(self, dc_identifier):
+        """
+        TODO
+        """
+        raise NotImplementedError("todo")  # TODO better msg, implement in subclasses
+
+    def _default_file_name(self):
+        """
+        TODO
+        """
+        raise NotImplementedError("todo")  # TODO better msg, implement in subclasses
+
+    def _construct_download_location(self, base_path, file_dir, file_name):
+        """
+        Return :class:`pathlib.Path`
+        """
+        if not base_path:
+            base_path = self._default_base_path
+        if not file_dir:
+            file_dir = self._default_file_dir
+        if not file_name:
+            file_name = self._default_file_name
+
+        return Path(base_path) / Path(file_dir) / Path(file_name)
+
+    def download(self, dc_identifier, base_path=None, file_dir=None, file_name=None):
+        """
+        Download the file from NLF.
+
+        :param dc_identifier: Dublin Core identifier for the binding to which this file
+            belongs. These identifiers are of form
+            https://digi.kansalliskirjasto.fi/sanomalehti/binding/[BINDING ID] and thus
+            the base of the download URL.
+        :type dc_identifier: String
+        :param base_path:
+        :type base_path:
+        :param file_dir:
+        :type file_dir:
+        :param file_name:
+        :type file_name:
+        """
+        output = self._construct_download_location(base_path, file_dir, file_name)
+        with urllib.request.urlopen(self.download_url(dc_identifier)) as source, open(
+            output, "w"
+        ) as output_file:
+            output_file.write(source.read())
 
 
 class UnknownTypeFile(File):
