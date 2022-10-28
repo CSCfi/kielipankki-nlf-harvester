2df63178d2187ccf0c85df2356f901ba9123d1d6
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-10-28 13:39:31 +0000

    KP-2606 Test DAG loading in unit tests

--- tests/pipeline/test_local_download_demo_dag.py
+++ tests/pipeline/test_local_download_demo_dag.py
@@ -1,5 +1,15 @@
+import pytest
+
 import pipeline.dags.local_download_demo
 
+from airflow.models import DagBag
 
-def test_nothing():
-    assert True
+
+@pytest.fixture()
+def dagbag():
+    return DagBag(dag_folder="pipeline/dags/local_download_demo.py")
+
+
+def test_dag_loading(dagbag):
+    dag = dagbag.get_dag(dag_id="download_altos_for_binding_to_local")
+    assert dag is not None
