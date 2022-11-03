cc37e1f54c014bff957dc9b4d5d5d6aaa6cbc07d
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-11-03 15:07:13 +0000

    KP-6208 Add test for a single operator

--- tests/pipeline/test_local_download_demo_dag.py
+++ tests/pipeline/test_local_download_demo_dag.py
@@ -1,8 +1,15 @@
-import pytest
+"""
+POC on how DAGs and their components can be tested
+"""
 
-import pipeline.dags.local_download_demo
+import os
 
 from airflow.models import DagBag
+import pytest
+
+from harvester import utils
+import pipeline.dags.local_download_demo
+from pipeline.dags.local_download_demo import SaveMetsOperator
 
 
 @pytest.fixture()
@@ -23,3 +30,26 @@ def test_dag_loading(dagbag):
     """
     dag = dagbag.get_dag(dag_id="download_altos_for_binding_to_local")
     assert dag is not None
+
+
+def test_save_mets_operator(
+    oai_pmh_api_url, mets_dc_identifier, expected_mets_response, tmp_path
+):
+    """
+    Check that executing save_mets_operator does indeed fetch a METS file
+    """
+    operator = SaveMetsOperator(
+        task_id="test_save_mets_task",
+        api_url=oai_pmh_api_url,
+        dc_identifier=mets_dc_identifier,
+        base_path=tmp_path,
+    )
+
+    operator.execute(context={})
+
+    binding_id = utils.binding_id_from_dc(mets_dc_identifier)
+    mets_location = tmp_path / "mets" / f"{binding_id}_METS.xml"
+
+    assert os.path.exists(mets_location)
+    with open(mets_location, "r", encoding="utf-8") as saved_mets:
+        assert saved_mets.read() == expected_mets_response
