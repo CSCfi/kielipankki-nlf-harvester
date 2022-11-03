0b6d8c5a94f038af004f929a484cc959efbd658d
Anni Järvenpää <anni.jarvenpaa@csc.fi>
2022-11-03 15:06:24 +0000

    KP-6208 Get the DagBag from a directory, add docstrings

--- tests/pipeline/test_local_download_demo_dag.py
+++ tests/pipeline/test_local_download_demo_dag.py
@@ -7,9 +7,19 @@ from airflow.models import DagBag
 
 @pytest.fixture()
 def dagbag():
-    return DagBag(dag_folder="pipeline/dags/local_download_demo.py")
+    """
+    DagBag containing the DAGs under pipeline/dags/.
+    """
+    return DagBag(dag_folder="pipeline/dags")
+
+
+# Pylint does not understand fixtures
+# pylint: disable=redefined-outer-name
 
 
 def test_dag_loading(dagbag):
+    """
+    Check that DAG can be parsed from the file
+    """
     dag = dagbag.get_dag(dag_id="download_altos_for_binding_to_local")
     assert dag is not None
