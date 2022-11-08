032937b9781fd410325791ee9ad32b945cad5d11
helhotti <helmiina.hotti@csc.fi>
2022-11-08 15:18:59 +0200

    KP-6213 Better tests for creating intermediate directories on a remote server

--- harvester/utils.py
+++ harvester/utils.py
@@ -17,7 +17,8 @@ def binding_id_from_dc(dc_identifier):
 
 def make_intermediate_dirs(sftp_client, remote_directory) -> None:
     """
-    Create all the intermediate directories in a remote host
+    Create all the intermediate directories in a remote host and ensure that
+    the download path is created correctly.
 
     :param sftp_client: A Paramiko SFTP client.
     :param remote_directory: Absolute Path of the directory containing the file
@@ -30,7 +31,7 @@ def make_intermediate_dirs(sftp_client, remote_directory) -> None:
         return
     try:
         sftp_client.chdir(remote_directory)
-    except OSError:
+    except IOError:
         dirname, basename = os.path.split(remote_directory.rstrip("/"))
         make_intermediate_dirs(sftp_client, dirname)
         sftp_client.mkdir(basename)
