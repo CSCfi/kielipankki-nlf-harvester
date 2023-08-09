36c5b1afcf1d1761dc5cb0ef6de28479655a5fe1
Sam Hardwick <sam.hardwick@csc.fi>
2023-08-09 10:47:37 +0300

    Add missing self.

--- pipeline/plugins/operators/custom_operators.py
+++ pipeline/plugins/operators/custom_operators.py
@@ -382,7 +382,7 @@ class StowBindingBatchOperator(BaseOperator):
         with ssh_hook.get_conn() as ssh_client:
             sftp_client = ssh_client.open_sftp()
             ignore_files_set = self.get_ignore_files_set(sftp_client)
-            batch, batch_num = batch_with_index
+            batch, batch_num = self.batch_with_index
             for dc_identifier in batch:
                 binding_id = utils.binding_id_from_dc(dc_identifier)
                 tmp_binding_path = (
