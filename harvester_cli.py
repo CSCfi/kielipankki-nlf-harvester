42d80a746e2bc6f56b912b65202552fe0f441b14
Sam Hardwick <sam.hardwick@csc.fi>
2025-08-15 13:40:52 +0300

    KP-10123 Add check_zip_integrity command to harvester_cli

--- harvester_cli.py
+++ harvester_cli.py
@@ -4,6 +4,7 @@ Command line interface for the harvester
 
 import click
 from pathlib import Path
+import zipfile
 
 from harvester.pmh_interface import PMH_API
 from harvester.mets import METS
@@ -132,6 +133,101 @@ def download_files_from(mets_file_path, collection_dc_identifier, encoding, base
         except NotImplementedError:
             click.echo(f"No download URL available for file {file.location_xlink}")
 
+
+@cli.command
+@click.argument("zip_file_path")
+@click.argument(
+    "binding_id_list_file",
+)
+@click.option(
+    "--guess_prefix",
+    default=True,
+    help="Use name of zip to guess which binding ids it covers",
+)
+def check_zip_integrity(zip_file_path, binding_id_list_file, guess_prefix):
+    """
+    For each ID in binding_id_list_file, check that there is a METS associated
+    with it, and that the files referenced by the METS are present. Check that
+    nothing else is present.
+    """
+
+    prefix = ""
+    if guess_prefix:
+        try:
+            prefix = zip_file_path[
+                zip_file_path.rindex("_") + 1 : zip_file_path.index(".zip")
+            ]
+            _ = int(prefix)
+        except Exception:
+            prefix = ""
+    binding_id_set = {
+        line.strip() for line in open(binding_id_list_file) if line.startswith(prefix)
+    }
+    seen_id_set = set()
+    errors_found = 0
+    binding_info_string = ""
+    if prefix != "":
+        binding_info_string = f" for bindings with prefix {prefix}"
+    click.echo(
+        f"Performing integrity check for {zip_file_path.split('/')[-1]}{binding_info_string}, expecting {len(binding_id_set)} bindings"
+    )
+    with zipfile.ZipFile(zip_file_path, "r") as zip_file:
+        zip_file_namelist = set(zip_file.namelist())
+        for zipinfo_obj in zip_file.infolist():
+            if zipinfo_obj.is_dir():
+                continue
+            file_path = zipinfo_obj.filename
+            try:
+                binding_id = file_path.split("/")[-3]
+            except IndexError:
+                click.echo(f"  malformed path: {file_path}")
+                errors_found += 1
+                continue
+            if file_path.endswith("_METS.xml"):
+                # Existing METS files are considered "seen bindings", and we check that referenced files exist
+                seen_id_set.add(binding_id)
+                basepath = "/".join(file_path.split("/")[:-2])
+                with zip_file.open(file_path, "r") as mets_file:
+                    mets = METS(
+                        f"https://digi.kansalliskirjasto.fi/sanomalehti/binding/{binding_id}",
+                        mets_file=mets_file,
+                        encoding="utf-8",
+                    )
+                    for contentfile in mets.files():
+                        if contentfile.filename.endswith(".xml"):
+                            if (
+                                f"{basepath}/alto/{contentfile.filename}"
+                                not in zip_file_namelist
+                            ):
+                                click.echo(
+                                    f"  {binding_id}: missing ALTO: {contentfile.filename}"
+                                )
+                        else:
+                            if (
+                                f"{basepath}/access_img/pr-{contentfile.filename}"
+                                not in zip_file_namelist
+                            ):
+                                click.echo(
+                                    f"  {binding_id}: missing img: {contentfile.filename}"
+                                )
+            else:
+                # Other files are normally handled in the METS check, but
+                # there might also be garbage
+                if binding_id not in binding_id_set:
+                    f"  {binding_id}: id not in list, but zip contains {file_path}"
+                    errors_found += 1
+    for missing_binding_id in binding_id_set.difference(seen_id_set):
+        click.echo(f"  {missing_binding_id}: missing METS")
+        errors_found += 1
+    for superfluous_binding_id in seen_id_set.difference(binding_id_set):
+        click.echo(superfluous_binding_id + ": extra METS (wasn't in binding list)")
+        errors_found += 1
+    click.echo(f"Found {errors_found} errors")
+
+
+# mets/
+# alto/
+# access_img/pr-
 
 if __name__ == "__main__":
     cli()  # pylint: disable=no-value-for-parameter
